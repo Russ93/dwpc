d19f52cc2ed920b24ef42c5d4c2209badfc78c62
Russ93 <russ93@fullsail.edu>
2014-04-04 05:16:52 -0400

    Init

---
+++ lab/day2/portfolio/js/main.js
@@ -0,0 +1,4 @@
+$('document').ready(function(){
+    $('.projects li').mouseenter(function(){$(this).css('border','solid 2px #385E8B')})
+    $('.projects li').mouseleave(function(){$(this).css('border','solid 2px #999')})
+})
